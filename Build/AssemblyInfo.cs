542fc9e4fb9ab6d0265dddee8bffa4c6a90dd83a
afterwise <afterwise@gmail.com>
2018-06-20 16:33:42 +0200

    Initial

---
+++ Build/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyCopyright("2017 Hapki")]
+[assembly: AssemblyCompany("Hapki")]
+
+[assembly: InternalsVisibleTo("Hapki.Editor")]
+
